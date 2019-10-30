843ef6f67c2c0858d22e0e78d5a3244c471cdf0d
Dilmurod <dilmurod199810@gmail.com>
2019-10-30 16:20:01 +0500

    Adding new class BasingEntity

---
+++ src/main/java/net/ptosalaite/gitoverview/BaseEntity.java
@@ -0,0 +1,30 @@
+package net.ptosalaite.gitoverview;
+
+
+
+public class BaseEntity {
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "BaseEntity{" +
+                "id=" + id +
+                '}';
+    }
+
+    public boolean isNew()
+    {
+        return (this.id == null);
+    }
+}
+
+
